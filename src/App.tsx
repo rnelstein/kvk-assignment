6d4b2a888e150ef8fe23b36519f16a112adfc603
Rinio Nelstein <r.nelstein@live.nl>
2024-07-06 13:44:48 +0200

    update github pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <header>
         <Navbar />
       </header>
