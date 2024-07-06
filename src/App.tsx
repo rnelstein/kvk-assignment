5aa42129cfaa105542be4a5976fc8e2f654ee3b4
Rinio Nelstein <r.nelstein@live.nl>
2024-07-06 14:02:32 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Details from './components/Details';
 import Navbar from './components/Navbar';
 
+function NoMatch() {
+  let location = useLocation();
+
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <Router basename={process.env.REACT_APP_BASE_PATH}>
+    <Router basename={process.env.REACT_APP_ROUTER_BASENAME}>
       <header>
         <Navbar />
       </header>
@@ -14,6 +26,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/company/:id" component={Details} />
+          <Route path="*">
+            <NoMatch />
+          </Route>
         </Switch>
       </main>
     </Router>
